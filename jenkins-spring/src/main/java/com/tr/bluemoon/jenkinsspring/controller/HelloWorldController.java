c1ef4c76fb98292a965dcdbd246543f11ab9c867
Tiago Marques <tiago.marques@thomsonreuters.com>
2019-11-27 13:25:39 -0300

    Add project spring with Jenkins

---
+++ jenkins-spring/src/main/java/com/tr/bluemoon/jenkinsspring/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.tr.bluemoon.jenkinsspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    private String get() {
+        return "Hello World";
+    }
+}
---
+++ jenkins-spring/src/test/java/com/tr/bluemoon/jenkinsspring/JenkinsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.bluemoon.jenkinsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
